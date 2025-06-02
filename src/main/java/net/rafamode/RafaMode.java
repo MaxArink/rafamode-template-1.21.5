aad05f92ff22f95fee3693d9a308b6782288d49c
MaxArink <maxarink2005@gmail.com>
2025-06-02 10:12:11 +0200

    Feat: new Files

---
+++ src/main/java/net/rafamode/RafaMode.java
@@ -0,0 +1,16 @@
+package net.rafamode;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RafaMode implements ModInitializer {
+	public static final String MOD_ID = "rafamode";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/rafamode/RafaModeClient.java
@@ -0,0 +1,10 @@
+package net.rafamode;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RafaModeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
